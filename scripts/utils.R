16e2de77666bfaa890ade9f0caade414bd3b4881
Guilherme Ribeiro <guilherme.fr@outlook.com>
2019-04-02 19:54:04 +0200

    Created functions 'runModels' and 'buildFormulas' for feature selection

--- scripts/utils.R
+++ scripts/utils.R
@@ -15,3 +15,54 @@ errorMetrics <- function(predictedValues, actualValues) {
   errors <- data.frame(absolute_error, MAE, relative_error, MRE, square_error, RMSE)
   errors
 }
+
+runModels <- function(dataset, dependentVarName, featuresName, method,
+                      percent = 0.75, seed = 123, trainControl, 
+                      tuneLength = NULL, tuneGrid = NULL, preProc = NULL) {
+  
+  partitions <- createTrainAndTestSets(dataset, dataset[, dependentVarName], percent, seed)
+  
+  formulas <- buildFormulas(dependentVarName, featuresName)
+  result <- vector("list", length(formulas))
+  i <- 1
+  for (formulaStr in formulas) {
+    formula <- formula(formulaStr)
+    modelFit <- train(formula, data = partitions$training, method = method, trControl = trainControl, 
+                      tuneLength = tuneLength, tuneGrid = tuneGrid, preProc = preProc)
+    
+    testPrediction <- predict(modelFit, partitions$testing)
+    testMetrics <- postResample(testPrediction, partitions$testing[ , dependentVarName])
+    testErrors <- errorMetrics(testPrediction, partitions$testing[ , dependentVarName])
+    result[[i]] <- list(method = method, formula = formulaStr, modelTrained = modelFit, 
+         testMetrics = testMetrics, testErrors = testErrors)
+    i <- i + 1
+  }
+  
+  result
+}
+
+buildFormulas <- function(dependentVarName, featuresName) {
+  formulas <- c()
+  
+  for( i in 1:length(featuresName) ) {
+    fCombinations <- combn(featuresName, i)
+    for (col in 1:ncol(fCombinations)) {
+      featureSet <- fCombinations[ , col]
+      
+      #The first feature is the most important one. It must be in all final formulas
+      if (featuresName[1] %in% featureSet) {
+        
+        #Beginning of the formula
+        formulaString <- paste(dependentVarName, "~", sep = "")
+        for (feature in featureSet) {
+          formulaString <- paste(formulaString, feature, "+", sep = " ")
+        }
+        #Removing the last '+'
+        formulaString <- substr(formulaString, 1, nchar(formulaString) - 2)
+        formulas <- append(formulas, formulaString)
+      }
+    }
+  }
+  
+  formulas
+}
